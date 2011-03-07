129501e06d823c6095c2f76b923598809ae5e17f
Elise Wursten <elise@wursten.be>
2011-03-07 14:56:33 +0100

    Fix loadfile

--- matlab/loadfile.m
+++ matlab/loadfile.m
@@ -2,5 +2,6 @@ function [data] = loadfile(filename)
 % Wrapper around the load command to easily load datasets in a seperate 
 % directory, without having to dick around with unix vs windows slashes.
 
-data = load(['../data/', filename]);
-%data = load(['..\data\', filename]);
+%data = load(['../data/', filename]);
+data = load(['..\data\', filename]);
+data = data.data;
