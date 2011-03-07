372fb4ebee18a3e72525b471b8a1d31c6221e295
Elise Wursten <elise@wursten.be>
2011-03-07 23:14:59 +0100

    Rectangular and tanh window

---
+++ matlab/rectWindow.m
@@ -0,0 +1,9 @@
+function [window] = rectWindow(spectrum,samplerate,leftcutoff,rightcutoff)
+% rectWindow returns a window which is 
+%   1 if the frequency lies between 'leftcutoff' and 'rightcutoff' 
+%   0 otherwise.
+fnyq = samplerate/2;
+f = linspace(-fnyq,fnyq,length(spectrum))';
+f = fftshift(f);
+window = ( leftcutoff < abs(f) < rightcutoff );
+
