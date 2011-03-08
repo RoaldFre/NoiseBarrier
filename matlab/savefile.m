40479d14713314d343cf5aa072e5a487d1bcaf4a
Elise Wursten <elise@wursten.be>
2011-03-08 14:36:22 +0100

    Fixed savefile

--- matlab/savefile.m
+++ matlab/savefile.m
@@ -2,5 +2,5 @@ function savefile(data, filename)
 % Wrapper around the save command to easily save datasets in a seperate 
 % directory, without having to dick around with unix vs windows slashes.
 
-save ['../data/',filename] -V7 data;
-%save ['..\data\',filename] -V7 data;
+%save ['../data/',filename] -V7 data;
+save(['..\data\',filename], 'data', '-V7');
