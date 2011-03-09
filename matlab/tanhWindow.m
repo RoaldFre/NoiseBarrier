9b5dc7e08a58776bbfd4aa1745915cc466651673
Roald Frederickx <roald.frederickx@gmail.com>
2011-03-09 15:28:09 +0100

    seperate window steepness for cut offs

--- matlab/tanhWindow.m
+++ matlab/tanhWindow.m
@@ -1,12 +1,12 @@
-function window = tanhWindow(f1, f2, transitionWidth, samplerate, n)
+function window = tanhWindow(f1, f2, width1, width2, samplerate, n)
 % Tanh window.
-% f1, f2:          Lower and upper frequency bounds.
-% transitionWidth: Width of the transition, measured in frequency. Controls 
-%                  the steepness of the window.
-% samplerate:      Samplerate of signal.
-% n:               Vumber of samples (should be even for the moment).
+% f1, f2:         Lower and upper frequency bounds.
+% width1, width2: Width of the transition, measured in frequency. Controls 
+%                 the steepness of the window.
+% samplerate:     Samplerate of signal.
+% n:              Vumber of samples (should be even for the moment).
 
 freqs = linspace(samplerate/n, samplerate/2, n/2)';
-win = (0.5 + 0.5*tanh((freqs - f1)/transitionWidth)) ...
-	- (0.5 + 0.5*tanh((freqs - f2)/transitionWidth));
+win = (0.5 + 0.5*tanh((freqs - f1)/width1)) ...
+	- (0.5 + 0.5*tanh((freqs - f2)/width2));
 window = [win; win(length(win):-1:1)];
