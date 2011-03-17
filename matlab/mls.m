50870d8d31d17524d6d05a29caee79792158d1d3
Roald Frederickx <roald.frederickx@gmail.com>
2011-03-17 20:41:18 +0100

    mls

---
+++ matlab/mls.m
@@ -0,0 +1,10 @@
+%guess where the sync pulse stops
+start=37000;
+
+micSpek = fft(mic(start:end));
+mlsPad = postpad(mls, length(micSpek);
+mlsPadRev = mlsPad(end:-1:1);
+mlsPadRevSpek = fft(mlsPadRev);
+
+impulse = ifft(micSpek .* mlsPadRevSpek);
+
