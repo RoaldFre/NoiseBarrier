637b21d808dbf6fda03913de7b8783882454cca0
Roald Frederickx <roald.frederickx@gmail.com>
2011-04-21 16:24:07 +0200

    see prev commit

--- matlab/impulseResponseFromMLSs.m
+++ matlab/impulseResponseFromMLSs.m
@@ -2,4 +2,4 @@ function impulseResponse = impulseResponseFromMLSs(signal, mls, n, numPreTrigger
 
 mlsPadded = postpad(mls, length(signal));
 impulseResponses = real(ifft(fft(signal) .* fft(mlsPadded(end:-1:1))));
-impulseResponse = averageConsecutive(impulseResponses, length(mls), n, 0.5, 2, numPreTrigger);
+impulseResponse = averageConsecutive(impulseResponses, length(mls), n, 0.5, 2, numPreTrigger, true);
--- matlab/impulseResponseFromSweeps.m
+++ matlab/impulseResponseFromSweeps.m
@@ -1,5 +1,6 @@
 function impulseResponse = impulseResponseFromSweeps(signal, sweep, n, numPreTrigger)
 
-sweepPadded = postpad(sweep, length(signal));
-impulseResponses = real(ifft(fft(signal) ./ fft(sweepPadded)));
-impulseResponse = averageConsecutive(impulseResponses, length(sweep), n, 0.5, 2, numPreTrigger);
+%sweepPadded = postpad(sweep, length(signal));
+%impulseResponses = real(ifft(fft(signal) ./ fft(sweepPadded)));
+impulseResponses = real(ifft(fft(signal) ./ fft(sweep, length(signal))));
+impulseResponse = averageConsecutive(impulseResponses, length(sweep), n, 0.5, 2, numPreTrigger, false);
