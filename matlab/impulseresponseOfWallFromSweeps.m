a8c78a69bd203fdcf8eb3352b885747600f43531
Roald Frederickx <roald.frederickx@gmail.com>
2011-04-22 11:23:43 +0200

    quick commit to share

--- matlab/impulseresponseOfWallFromSweeps.m
+++ matlab/impulseresponseOfWallFromSweeps.m
@@ -7,7 +7,8 @@ sweep = logsweep(50,10000,1,96000);
 
 flatLength = 8; %in ms
 
-sweeps = loadfile([dir,'/',signal,'-vrij-h315-rsm300-gras-1']);
+%free field
+sweeps = loadfile([dir,'/',signal,'-vrij-h315-rsm300-gras-2']);
 sweeps = sweeps(:,1);
 
 IRfree = impulseResponseFromSweeps(sweeps, sweep, numberOfAverages, numPreTrigger);
@@ -24,12 +25,11 @@ IR = impulseResponseFromSweeps(sweeps, sweep, numberOfAverages, numPreTrigger);
 
 [IRwindowed, window, unwindowed] = adrienneWindow(IR, flatLength, 96000);
 
-tanhWind = tanhWindow(50, 10000, 10, 4000, 96000, length(IRwindowed));
-IRdeconv = ifft(fft(IRwindowed) ./ fft(IRfreeWindowed));
-IRdeconvTanh = ifft(fft(IRwindowed) ./ fft(IRfreeWindowed) .* tanhWind);
 
-IRsynced = syncdirect(IRfreeWindowed, IRwindowed, 0.0010, 0.0005, 96000, 7, true);
+%% subtracting
+IRsynced = syncdirect(IRfreeWindowed, IRwindowed, 0.0010, 0.0005, 96000, 5, false);
 
+figure;
 clf; hold on;
 plot(IRsynced);
 plot(IRfreeWindowed, 'g');
@@ -41,8 +41,24 @@ IRsubtracted = IRsynced - IRfreeWindowed;
 IRsubtractedWindowed = adrienneTillEnd(IRsynced, 0.0, 96000);
 
 spectrum = abs(fft(IRsubtractedWindowed, length(IRwindowed)) ./ fft(IRwindowed));
-plot(spectrum, '-x');
+f = linspace(0, 96000, length(spectrum));
+figure;
+plot(f, spectrum, '-x');
 
-axis([0, length(spectrum)/96000*10000, 0, 1], 'autoy');
+axis([0, 10000, 0, 1], 'autoy');
+
+
+%% deconvolve
+IRwindowedPadded = prepad(IRwindowed, length(IRwindowed) * 1.1);
+IRfreeWindowedPadded = postpad(IRfreeWindowed, length(IRwindowedPadded));
+
+tanhWind = tanhWindow(50, 4000, 20, 1000, 96000, length(IRwindowedPadded));
+IRdeconv = ifft(fft(IRwindowedPadded) ./ fft(IRfreeWindowedPadded));
+IRdeconvTanh = ifft(fft(IRwindowedPadded) ./ fft(IRfreeWindowedPadded) .* tanhWind);
+
+
+
+figure;
+plot(IRdeconvTanh);
 
 
