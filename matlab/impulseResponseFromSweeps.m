3214142c0db1c7c3c8faecc9249b7794b37750f6
Elise Wursten <elise@wursten.be>
2011-05-02 15:54:16 +0200

    Haakje vergeten

--- matlab/impulseResponseFromSweeps.m
+++ matlab/impulseResponseFromSweeps.m
@@ -1,6 +1,6 @@
 function impulseResponse = impulseResponseFromSweeps(signal, sweep, n, numPreTrigger, cutoffFreq1, cutoffFreq2, cutoffWidth1, cutoffWidth2, samplerate)
 
 window = tanhWindow(cutoffFreq1, cutoffFreq2, cutoffWidth1, cutoffWidth2, samplerate, length(signal));
-impulseResponses = real(ifft(fft(signal) ./ fft(sweep, length(signal) .* window)));
+impulseResponses = real(ifft(fft(signal) ./ fft(sweep, length(signal)) .* window));
 
 impulseResponse = averageConsecutive(impulseResponses, length(sweep), n, 0.5, 2, numPreTrigger, false);
