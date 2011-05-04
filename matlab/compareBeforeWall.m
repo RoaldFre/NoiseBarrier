926c78cf5368e5558d843a49f2d6ab4720b9da7b
Roald Frederickx <roald.frederickx@gmail.com>
2011-05-04 02:18:48 +0200

    forgot this one

---
+++ matlab/compareBeforeWall.m
@@ -0,0 +1,37 @@
+[simulated, simTime, simSpectra, simFreqs, simFreeField, simDeconvolved] = processSimulationBeforeWall;
+[measurements, time, spectra, measurementFreqs, freeField, deconvolved, measurementsCorrected, spectraCorrected, deconvolvedCorrected] = processBeforeWall;
+
+beforeWallData;
+
+tMin = time(1);
+tMax = time(end);
+c = 340;
+
+for x = 0 : sideSteps - 1
+	for y = 0 : upSteps - 1
+		i = 1 + upSteps*x + y;
+		iMeasurement = i;
+		iSimulation = i;
+		
+		clf;
+		subplot(2,1,1);
+		hold on;
+		semilogx(measurementFreqs, 20*log10(spectra(:,iMeasurement)), 'r');
+		semilogx(measurementFreqs, 20*log10(spectraCorrected(:,iMeasurement)), 'g');
+		semilogx(simFreqs, 20*log10(simSpectra(:,iMeasurement)), 'k');
+		title(['x = ',num2str(x),'   y = ',num2str(y),'   i = ',num2str(iMeasurement)])
+		axis([2000, 50000, -60, 20]);
+		
+		subplot(2,1,2);
+		hold on;
+		plot(time, deconvolved(:,iSimulation), 'r');
+		plot(time, deconvolvedCorrected(:,iSimulation), 'g');
+		plot(simTime, simDeconvolved(:,iSimulation), 'k');
+		axis([tMin, tMax, -0.004, 0.006],'autoy');
+
+		pause(2);
+	end
+	pause(3);
+end
+hold off;
+
